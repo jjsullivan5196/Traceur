a68fd96df53c3f502a65c19d3589f343264edfbe
Vic Arreola <varreola@csumb.edu>
2017-04-01 13:14:44 -0700

    Planning

---
+++ Assets/Scripts/MoInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MoInput {
+    public delegate void InputEvent();
+    public static event InputEvent StepRight;
+    public static event InputEvent StepLeft;
+    public static event InputEvent Jump;
+    public static event InputEvent Duck;
+
+    public static void EvStepRight() { StepRight(); }
+    public static void EvStepLeft() { StepLeft(); }
+    public static void EvJump() { Jump(); }
+    public static void EvDuck() { Duck(); }
+}
+
+/*public class MyDemo
+{
+    void EventDo()
+    {
+        Debug.Log("I did something");
+        MoInput.EvStepRight();
+    }
+    
+    public MyDemo()
+    {
+        MoInput.StepLeft += EventDo;
+    }
+}
+*/
