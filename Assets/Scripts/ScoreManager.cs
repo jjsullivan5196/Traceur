68e571188bc9bbe778fc4f8da2afef4e6cc058ed
Brian Little <blittle@csumb.edu>
2017-04-06 14:05:12 -0700

    Added flickering on collision and score multiplier.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections;
+﻿/*
+ Name: Brian Little
+ Class: CST 306
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,12 +11,14 @@ public class ScoreManager : MonoBehaviour {
 
     public Text text;
     private bool allowPoints;
-    private float cooldown;
+	private float pntsCooldown;
     private float startPosition;
     private float score;
+	private int multiplier = 1;
 
 	// Use this for initialization
 	void Start () {
+		text = GameObject.FindGameObjectWithTag ("ScoreLabel").GetComponent<Text>();
         allowPoints = true;
         startPosition = transform.position.z;
         score = 0;
@@ -20,14 +26,16 @@ public class ScoreManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(cooldown > 0)
+        if(pntsCooldown > 0)
         {
-            cooldown -= Time.deltaTime;
-            Debug.Log(cooldown);
+            pntsCooldown -= Time.deltaTime;
+            //Debug.Log(pntsCooldown);
+
         }
         else
         {
             allowPoints = true;
+			GameObject.FindGameObjectWithTag ("Flicker").GetComponent<Image> ().enabled = false;
         }
 
 		if(allowPoints)
@@ -39,17 +47,57 @@ public class ScoreManager : MonoBehaviour {
 
     private void OnTriggerEnter(Collider col)
     {
-        allowPoints = false;
-        cooldown = 5.0f;
-        //Debug.Log(cooldown);
+		if(col.gameObject.tag == "Enemy"){
+	        allowPoints = false;
+			pntsCooldown = 5.0f;
+			GameObject.FindGameObjectWithTag ("Flicker").GetComponent<Image> ().enabled = true;
+			StartCoroutine ("blink");
+	        //Debug.Log(cooldown);
+		}
+		if (col.gameObject.tag == "Multiplier") {
+			StartCoroutine ("adjustScore");
+			Destroy (col);
+		}
     }
 
     private void calculateScore()
     {
         float distance = transform.position.z - startPosition;
-        if (distance > 1) {
+        if (distance >= 1) {
             startPosition = Mathf.Floor(transform.position.z);
-            score += 10;
+            score += 10 * multiplier;
         }
     }
+
+	//Coroutine allows screen to blink once every .5 seconds
+	private IEnumerator blink()
+	{
+		float cd = 5.0f;
+		float blinkInterval = .5f;
+		while (cd > 0) {
+			Debug.Log (blinkInterval);
+			cd -= Time.deltaTime;
+			blinkInterval -= Time.deltaTime;
+
+			if (blinkInterval <= 0) {
+				blinkInterval = .5f;
+				if(GameObject.FindGameObjectWithTag ("Flicker").GetComponent<Image> ().enabled == true)
+					GameObject.FindGameObjectWithTag ("Flicker").GetComponent<Image> ().enabled = false;
+				else
+					GameObject.FindGameObjectWithTag ("Flicker").GetComponent<Image> ().enabled = true;
+			}
+			yield return null;
+		}
+	}
+
+	private IEnumerator adjustScore()
+	{
+		float cd = 5.0f;
+		while (cd > 0) {
+			cd -= Time.deltaTime;
+			multiplier = 2;
+			yield return null;
+		}
+		multiplier = 1;
+	}
 }
