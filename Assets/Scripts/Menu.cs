d88ecddbe50ea3316b55bf993715c5763d1df6f1
John Sullivan <jsullivan@csumb.edu>
2017-05-05 22:15:22 -0700

    Milestone 4

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,85 @@
+﻿/*
+
+name: Anthony Truong
+
+course: CST306
+
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+    public Canvas PlayMenu;
+    public Canvas OptionMenu;
+
+    public Button Play;
+    public Button Option;
+    public Button Quit;
+    public Button Live;
+    public Button Distance;
+
+    private void Start()
+    {
+        PlayMenu = PlayMenu.GetComponent<Canvas>();
+        Play = Play.GetComponent<Button>();
+        Quit = Quit.GetComponent<Button>();
+        Live = Live.GetComponent<Button>();
+        Distance = Distance.GetComponent<Button>();
+        PlayMenu.enabled = false;
+        OptionMenu.enabled = false;
+    }
+
+    public void clickOnPlay()
+    {
+        PlayMenu.enabled = true;
+        Play.enabled = false;
+        Option.enabled = false;
+        Quit.enabled = false;
+    }
+
+    public void clickOnOption()
+    {
+        OptionMenu.enabled = true;
+        Play.enabled = false;
+        Option.enabled = false;
+        Quit.enabled = false;
+    }
+
+    public void clickOnBackPlay()
+    {
+        PlayMenu.enabled = false;
+        Play.enabled = true;
+        Option.enabled = true;
+        Quit.enabled = true;
+    }
+
+    public void clickOnBackOption()
+    {
+        Debug.Log("BackOption");
+        OptionMenu.enabled = false;
+        Play.enabled = true;
+        Option.enabled = true;
+        Quit.enabled = true;
+    }
+
+    public void clickOnExit()
+    {
+        Application.Quit();
+    }
+
+    public void clickOnLive()
+    {
+        //LaunchSceneLive
+        Debug.Log("Launch Scene Live");
+    }
+
+    public void clickOnDistance()
+    {
+        //LaunchSceneDistance
+        Debug.Log("Launch Scene Distance");
+    }
+}
---
+++ Assets/Scripts/ObstacleMove.cs
@@ -0,0 +1,37 @@
+﻿/*
+ name: Brian Little
+ course: CST306
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMove : MonoBehaviour {
+
+	private Vector3 target;
+	private bool moveL;
+	// Use this for initialization
+	void Start () {
+		//target = gameObject.position;
+		moveL = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		target = gameObject.transform.position;
+
+		if (moveL) {
+			target.x = -3;
+			if (gameObject.transform.position.x < -2.9f) {
+				moveL = false;
+			}
+		} else {
+			target.x = 3;
+			if (gameObject.transform.position.x > 2.9f) {
+				moveL = true;
+			}
+		}
+
+		transform.position = Vector3.Lerp(gameObject.transform.position, target, .05f);
+	}
+}
